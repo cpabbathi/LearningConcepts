4472505597423bb6078fbfeb23afcfc81af19c78
Chaithra Pabbathi <chaithrap@gmail.com>
2020-11-12 08:52:37 -0600

    Adding ScreenComposition

---
+++ ScreenComposition/ScreenComposition/ScreenCompositionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenCompositionApp.swift
+//  ScreenComposition
+//
+//  Created by Chaithra Pabbathi on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ScreenCompositionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
